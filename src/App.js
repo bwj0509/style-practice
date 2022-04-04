3666d549a250179b441dbf31876d86140424a1d2
Woojin <bwj59@naver.com>
2022-04-05 00:55:16 +0900

    ㅔㄱ

--- src/App.js
+++ src/App.js
@@ -4,20 +4,30 @@ import Button from './components/Button';
 function App() {
   return (
     <div className='App'>
-      <div>
-        <Button size="large">Button</Button>
+      <div className='buttons'>
+        <Button size="large" onClick={() => console.log('클릭했습니다.')}>Button</Button>
         <Button>Button</Button>
         <Button size="small">Button</Button>
       </div>
-      <div>
+      <div className='buttons'>
         <Button size="large" color="gray">Button</Button>
         <Button color="gray">Button</Button>
         <Button size="small" color="gray">Button</Button>
       </div>
-      <div>
+      <div className='buttons'>
         <Button size="large" color="pink">Button</Button>
         <Button color="pink">Button</Button>
         <Button size="small" color="pink">Button</Button>
+      </div>
+      <div className='buttons'>
+        <Button size="large" outline>Button</Button>
+        <Button color="gray" outline>Button</Button>
+        <Button size="small" color="pink" outline>Button</Button>
+      </div>
+      <div className='buttons'>
+        <Button size="large" fullWidth>Button</Button>
+        <Button size="large" color="gray" fullWidth>Button</Button>
+        <Button size="large" color="pink" fullWidth>Button</Button>
       </div>
     </div>
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './Button.scss'
 import classNames from 'classnames';
 
-function Button({ children, size, color }) {
+function Button({ children, size, color, outline, fullWidth, ...rest }) {
     return (
-        <button className={classNames('Button', size, color)}>{children}</button>
+        <button className={classNames('Button', size, color, { outline, fullWidth })} {...rest}>
+            {children}
+        </button>
     );
 }
 
